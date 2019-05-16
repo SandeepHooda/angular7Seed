e3e0c7fa25292f1bfaed47f7858b12b637210c09
SandeepHooda <sonu.hooda@gmail.com>
2019-05-16 22:48:33 +0530

    seed with prime ng

---
+++ seed-app/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './comp/home/home.component';
+import { CashComponent } from './comp/cash/cash.component';
+import { LoginComponent } from './comp/login/login.component';
+
+import {AccordionModule} from 'primeng/accordion';    
+import {MenuItem} from 'primeng/api';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CashComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+        TableModule,
+        HttpClientModule,
+        InputTextModule,
+        DialogModule,
+        ButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
